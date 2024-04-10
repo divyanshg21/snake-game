e94215127db3ffdd5e69efd64ae502e910d021f0
[Divyansh Gupta] <[divyanshgupta2003@gmail.com]>
2024-04-10 05:53:05 +0530

    flask

--- main.py
+++ main.py
@@ -1,53 +1,56 @@
+import os
+from flask import Flask, render_template
 from turtle import Screen, Turtle
 from snake import Snake
 import time
 from food import Food
 from scoreboard import Scoreboard
 
+app = Flask(__name__)
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("Snake Game")
-screen.tracer(0)
+@app.route('/')
+def run_game():
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("Snake Game")
+    screen.tracer(0)
 
-snake=Snake()
-food = Food()
-scoreboard = Scoreboard()
+    snake = Snake()
+    food = Food()
+    scoreboard = Scoreboard()
 
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
 
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(0.07)
+        snake.move()
 
-game_is_on=True
-while game_is_on:
-    screen.update()
-    time.sleep(0.07)
+        # Detect collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
 
-    snake.move()
-
-    #detect collision with food
-    if snake.head.distance(food)< 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
-
-    #detect collision with wall
-    if snake.head.xcor()>280 or snake.head.xcor() < -300 or snake.head.ycor()>300 or snake.head.ycor()<-280:
-        scoreboard.reset()
-        snake.reset()
-        
-    
-    #detect collision with tail
-
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment)<10:
-            
+        # Detect collision with wall
+        if snake.head.xcor() > 280 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -280:
             scoreboard.reset()
             snake.reset()
 
+        # Detect collision with tail
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                scoreboard.reset()
+                snake.reset()
 
-screen.exitonclick()
+    screen.exitonclick()
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
